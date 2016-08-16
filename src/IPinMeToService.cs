f382b6ed4ec23afa09b7e1366a6ef66aff119b01
Per Magne Skuseth <permagne@bvnetwork.com>
2016-08-16 14:38:47 +0200

    Minor cleanup

--- src/IPinMeToService.cs
+++ src/IPinMeToService.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
-using PinMeTo.Net.Models;
 
 namespace PinMeTo.Net
 {
--- src/PinMeToService.cs
+++ src/PinMeToService.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
-using PinMeTo.Net.Models;
 
 namespace PinMeTo.Net
 {
